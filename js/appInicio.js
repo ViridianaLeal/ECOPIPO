e99cd292c4c60a5912fd07075b8f170127719683
Viridiana Leal <LERV060816MHGLMRA8@soycecytem.mx>
2023-12-04 22:32:47 -0600

    cuatro

--- js/appInicio.js
+++ js/appInicio.js
@@ -2,3 +2,11 @@ $('button').on('click', function(e){
     e.preventDefault();
     $(this).toggleClass('is-expanded');
   })
+
+
+  function classToggle() {
+    var el = document.querySelector('.icon-cards__content');
+    el.classList.toggle('step-animation');
+  }
+  
+  document.querySelector('#toggle-animation').addEventListener('click', classToggle);
